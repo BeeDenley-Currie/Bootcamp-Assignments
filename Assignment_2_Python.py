bb39c04f16b11e26d3dd8d95ee26336d21c51cf4
Bee <bee.denley@outlook.com>
2024-09-12 19:35:41 +0100

    Update Assignment_2_Python.py
    Updated code slightly, adding more comments on reasons for choosing different modules, packages, and functions.

--- Assignment_2_Python.py
+++ Assignment_2_Python.py
@@ -1,3 +1,4 @@
+# these 2 modules are required. Neither should need to be installed, but they do need importing.
 import random
 import requests
 
@@ -14,22 +15,23 @@ def instructions():
     print("1. You will enter your chosen date")
     print("2. Choose whether you would like to see a historical event, birth, or death")
     print("3. A fun fact is printed, and written to a log .txt file")
-    print("You then have the option to rerun with another date or to end the program")
+    print("4. You then have the option to rerun with another date or to end the program")
     print("Historical data provided by https://zenquotes.io/")
     print("Enjoy!"+"\n")
 
 def date_info():
     # user inputs date as string, use string slicing to extract necessary parts for URL
     # Gets the zenquotes On This Day API. No need to log in/authenticate
-    # creates a dictionary that contains the facts to print
     date = input("What date would you like to search? Use format DD/MM: ")
     day = date[0:2]
     month = date[3:5]
+    #variable needs to be global so it can be used outside the function
     global on_day
     on_day = f"Day: {day}, Month: {month}"
     url = 'https://today.zenquotes.io/api/{}/{}'.format(month, day)
     response = requests.get(url)
     data = response.json()
+    # creates a dictionary that contains the facts to print
     return {
         'Event': data['data']['Events'],
         'Birth': data['data']['Births'],
@@ -43,20 +45,21 @@ def choice():
         print("Sorry, I need the format either Event, Birth, or Death")
         choice()
     else:
-        # using the length of the option list to form the range for random number generation. import random at start allows us to use randrange
+        # using the length of the option list to form the range for random number generation.
         random_index = random.randrange(len(my_date[option]))
         # get the value for the text key in this random dictionary
         random_item = my_date[option][random_index]['text']
         # split on a character used in all the text json fields, normally to give colour information
         # this also removes these unneeded characters - bonus!
         output_list = random_item.split(" &#8211; ")
-        #if statement to add different value to fun_fact variable depending on option chosen, cuts down on recursive code
+        # if statement to add different value to fun_fact variable depending on option chosen, cuts down on recursive code
+        # I wanted to use ANSI escape code to change the year colour, but that doesn't work with writing to a .txt file.
         if option == "Event":
-                fun_fact = f"Fun fact! On this day in {output_list[0]}, {output_list[1]}"+"\n"
+            fun_fact = f"Fun fact! On this day in {output_list[0]}, {output_list[1]}" + "\n"
         elif option == "Birth":
-                fun_fact = f"Fun Fact! On this day in {output_list[0]}, {output_list[1]} was born"+"\n"
+            fun_fact = f"Fun Fact! On this day in {output_list[0]}, {output_list[1]} was born" + "\n"
         elif option == "Death":
-                fun_fact = f"...somewhat Fun Fact! On this day in {output_list[0]}, {output_list[1]} died"+"\n"
+            fun_fact = f"...somewhat Fun Fact! On this day in {output_list[0]}, {output_list[1]} died" + "\n"
         # originally I only wrote to the text file, but it wasn't immediately clear that everything had worked.
         # this way it's more obvious if there's an error, and is also a reminder to view the log file
         print(fun_fact + "\nThis has been written to the On This Day log\n")
@@ -64,14 +67,15 @@ def choice():
             otd.write(fun_fact)
             otd.seek(0, 2)
 
-# Function for running the On This Day output
 def run():
+    # Function for running the On This Day output. Handy to have so the code down below is really simple!
     global my_date
     my_date = date_info()
     with open("on_this_day.txt", "a+") as otd:
         otd.write(on_day+"\n")
         otd.seek(0,2)
     choice()
+print("\n\033[92m You Win!\033[00m")
 
 instructions()
 run()
